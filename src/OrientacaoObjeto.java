efcbd3f370a8465460df6ddd271cc038cfd3d9db
Victor Vianna <victorvianna.hs@gmail.com>
2026-03-27 19:33:36 -0300

    refatoracao usando orientacao a objetos

--- src/OrientacaoObjeto.java
+++ src/OrientacaoObjeto.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 import java.util.Locale;
+import Entidade.Triangulo;
 
 public class OrientacaoObjeto {
     public static void main (String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double lado1, lado2, lado3;
-        double lado4, lado5, lado6;
-        double p1, p2;
-        double area1, area2;
+        Triangulo x,y;
+        x = new Triangulo();
+        y = new Triangulo();
+
 
         System.out.println("Digite os lados do Triangulo 1: ");
+        double lado1 = sc.nextDouble();
+        double lado2 = sc.nextDouble();
+        double lado3 = sc.nextDouble();
+        x.lados(lado1, lado2, lado3);
+
+        System.out.println("Digite os lados do Triangulo 2: ");
         lado1 = sc.nextDouble();
         lado2 = sc.nextDouble();
         lado3 = sc.nextDouble();
+        y.lados(lado1, lado2, lado3);
 
-        System.out.println("Digite os lados do Triangulo 2: ");
-        lado4 = sc.nextDouble();
-        lado5 = sc.nextDouble();
-        lado6 = sc.nextDouble();
-
-        p1 = (lado1 + lado2 + lado3) / 2.0;
-        p2 = (lado4 + lado5 + lado6) / 2.0;
-
-        area1 = Math.sqrt(p1 * (p1 - lado1) * (p1 - lado2) * (p1 - lado3));
-        area2 = Math.sqrt(p2 * (p2 - lado4) * (p2 - lado5) * (p2 - lado6));
+        double area1 = x.area();
+        double area2 = y.area();
 
         System.out.printf("Area Triangulo 1: %.4f%n", area1);
         System.out.printf("Area Triangulo 2: %.4f%n", area2);
